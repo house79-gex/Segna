1cb522b6203222fd1795fd9008355b800a1995d1
house79-gex <75929529+house79-gex@users.noreply.github.com>
2026-01-25 19:10:24 +0100

    Create WatchReceiverTheme.kt

---
+++ wear_os_app/app/src/main/java/com/example/watchreceiver/ui/theme/WatchReceiverTheme.kt
@@ -0,0 +1,13 @@
+package com.example.watchreceiver.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WatchReceiverTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
